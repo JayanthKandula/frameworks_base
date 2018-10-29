77a8fb9180ee5de8f3ef10c539a48f3d7fda91bc
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-10-28 21:54:21 -0300

    Fix screen unpinning text on devices that have gestures on fp sensor
    Change-Id: I0666acb0e8d87224c95ffeea78e07cfc0f05d3ae

--- packages/SystemUI/src/com/android/systemui/recents/ScreenPinningRequest.java
+++ packages/SystemUI/src/com/android/systemui/recents/ScreenPinningRequest.java
@@ -309,7 +309,7 @@ public class ScreenPinningRequest implements View.OnClickListener,
                 mLayout.findViewById(R.id.screen_pinning_home_bg_light).setVisibility(INVISIBLE);
                 mLayout.findViewById(R.id.screen_pinning_home_bg).setVisibility(INVISIBLE);
                 descriptionStringResId = !hasNavigationBar()
-                        ? R.string.screen_pinning_description_no_navbar
+                        ? (supportsGesturesOnFP() ? R.string.screen_pinning_description_no_navbar_fpsensor : R.string.screen_pinning_description_no_navbar)
                         : touchExplorationEnabled
                         ? R.string.screen_pinning_description_accessible
                         : R.string.screen_pinning_description;
@@ -318,7 +318,7 @@ public class ScreenPinningRequest implements View.OnClickListener,
                 mLayout.findViewById(R.id.screen_pinning_home_bg_light).setVisibility(VISIBLE);
                 mLayout.findViewById(R.id.screen_pinning_home_bg).setVisibility(VISIBLE);
                 descriptionStringResId = !hasNavigationBar()
-                        ? R.string.screen_pinning_description_no_navbar
+                        ? (supportsGesturesOnFP() ? R.string.screen_pinning_description_no_navbar_fpsensor : R.string.screen_pinning_description_no_navbar)
                         : touchExplorationEnabled
                         ? R.string.screen_pinning_description_recents_invisible_accessible
                         : R.string.screen_pinning_description_recents_invisible;
@@ -414,6 +414,11 @@ public class ScreenPinningRequest implements View.OnClickListener,
                 // ignore
             }
             return false;
+        }
+
+        private boolean supportsGesturesOnFP() {
+            return mContext.getResources().getBoolean(
+                org.lineageos.platform.internal.R.bool.config_supportsGesturesOnFingerprintSensor);
         }
 
         @Override
